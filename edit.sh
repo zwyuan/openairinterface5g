901e16f52d65df69f99925c9ba651e4682a7b045
Zengwen Yuan <zyuan@cs.ucla.edu>
2017-10-15 10:21:32 -0700

    feat: [DPCM] utilities for easy debugging

---
+++ edit.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+rsub /home/wing/oai/openairinterface5g/openair2/RRC/LITE/rrc_eNB.c &
+rsub /home/wing/oai/openairinterface5g/openair2/RRC/LITE/rrc_UE.c &
+rsub /home/wing/oai/openairinterface5g/openair2/RRC/LITE/defs.h &
+rsub /home/wing/oai/openairinterface5g/openair2/RRC/LITE/MESSAGES/asn1_msg.c &
+rsub /home/wing/oai/openairinterface5g/openair2/RRC/LITE/MESSAGES/asn1c/ASN1_files/RRC-e10.asn &
+
